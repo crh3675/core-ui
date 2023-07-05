a039cc873c3eb6f0de2a20e340fea7c993cb03ba
Hoovinator <crh3675@gmail.com>
2023-07-05 19:13:30 -0400

    Update prestart.js

--- prestart.js
+++ prestart.js
@@ -6,7 +6,9 @@ const path = require('path');
 const fs = require('fs');
 const app = require('./package.json');
 const colors = require('colors');
+const knex = require('knex');
 const rcfilepath = path.join(process.cwd(), `.${app.name}rc`);
+const knexfile = path.join(process.cwd(), `knexfile.js`);
 
 // Remove previous rc config to prevent from being
 // loaded by rc() library when we load the current config
@@ -16,12 +18,16 @@ if (fs.existsSync(rcfilepath)) {
 
 // Load the config defaults with local settings
 const config = require(path.join(process.cwd(), 'lib', 'config'));
+const params = findParams(config);
+const values = params.map(param => param.value);
 
 // Initialize AWS
-AWS.config.update(config.aws.creds.default);
+if(process.IS_AWS !== 'true') {
+  AWS.config.update(config.aws.creds.default);
+}
 
 /**
- * Iterate an object to find values matchins ssm
+ * Iterate an object to find values matching ssm
  * @param {Object} obj
  * @param {String} path
  * @return {None}
@@ -45,42 +51,68 @@ function findParams(obj, path) {
   return params;
 }
 
-const params = findParams(config);
-const values = params.map(param => param.value);
-const ssm = new AWS.SSM();
+/**
+ * Write a config file
+ * @param {Object} config
+ * @param {String} filePath
+ */
 const writeConfig = function(config, filePath) {
   // Create rc() file which will be auto-loaded in lib/config.js
   // https://github.com/dominictarr/rc
   fs.writeFileSync(filePath, JSON.stringify(config, null, '  '));
   console.log(`Successfully wrote configuration file ${filePath.green}`);
-  process.exit(0);
 };
 
-if (values.length) {
-  // Get SSM parameters from Amazom
-  ssm.getParameters({
-    Names: values,
-    WithDecryption: true
-  }, (err, data) => {
-    if (err) {
-      throw new Error(err);
-    }
-
-    // Re-map optained values to config
-    params.forEach(param => {
-      const ssmparam = data.Parameters.find(ssm => ssm.Name === param.value);
-
-      // Replace config value with SSM value
-      if (ssmparam) {
-        const setter = new Function('config', 'value', 'config.' + param.path + '=value;');
-        setter(config, ssmparam.Value);
-      } else {
-        console.error(`Failed to find SSM Param for ${param.path.red}`);
-        process.exit(1);
-      }
-    });
-    writeConfig(config, rcfilepath);
-  });
-} else {
-  writeConfig(config, rcfilepath);
+/**
+ * Run database migrations for latest
+ * @param {Object} config
+ */
+const migrateDatabase = function(config) {
+  knex(config).migrate.latest().then(() => {
+    process.exit(0);
+  }).catch(err => {
+    console.error(err);
+    process.exit(1);
+  })
 }
+
+/**
+ * Initialize application settings and update database
+ * @param {Object} values
+ */
+const initialize = function() {
+  // Main code
+  if (values.length) {
+    // Get SSM parameters from Amazom
+    const ssm = new AWS.SSM();
+    ssm.getParameters({
+      Names: values,
+      WithDecryption: true
+    }, (err, data) => {
+      if (err) {
+        throw new Error(err);
+      }
+
+      // Re-map obtained values to config
+      params.forEach(param => {
+        const ssmparam = data.Parameters.find(ssm => ssm.Name === param.value);
+
+        // Replace config value with SSM value
+        if (ssmparam) {
+          const setter = new Function('config', 'value', 'config.' + param.path + '=value;');
+          setter(config, ssmparam.Value);
+        } else {
+          console.error(`Failed to find SSM Param for ${param.path.red}`);
+          process.exit(1);
+        }
+      });
+      writeConfig(config, rcfilepath);
+      migrateDatabase(config.database);
+    });
+  } else {
+    writeConfig(config, rcfilepath);
+    migrateDatabase(config.database);
+  }
+}
+
+initialize();
